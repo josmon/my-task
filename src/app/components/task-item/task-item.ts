6059c2addf6e318fecb694810bc0a2575895bdcf
Josmon K T <josmonkuttikkat@gmail.com>
2025-11-27 06:53:33 +0530

    Task complete status update done

--- src/app/components/task-item/task-item.ts
+++ src/app/components/task-item/task-item.ts
@@ -10,11 +10,13 @@ export class TaskItem {
   @Input() task:String='';
   @Input() index=0;
   @Output() taskDeleted=new EventEmitter<number>();
+  @Output() taskCompleted=new EventEmitter<number>();
   
 
   isCompleted=false;
   toggleComplete(){
     this.isCompleted=!this.isCompleted;
+    this.taskCompleted.emit(this.index);
   }
 
   deleteTask(){
